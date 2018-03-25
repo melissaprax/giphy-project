063be7d08cf19cae95ef3e70e3f1092726d53ea5
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-03-25 15:51:40 -0500

    sun at 3:50

--- js/main.js
+++ js/main.js
@@ -1,6 +1,63 @@
 $( document ).ready(function() {
 
+//Gif array (for inital state buttons)
+let moods = ["happy", "bummed", "sassy", "dance"]
+
 //API Key: BIfCy45pXxtSMHZmseg1sl3mX3WxDuYV
+
+function showTheGifs() {
+
+let moodChoice = $(this).attr("gif-name")
+let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BIfCy45pXxtSMHZmseg1sl3mX3WxDuYV&q=" + moodChoice + "&limit=25&offset=0&rating=G&lang=en";
+
+//Jquery AJAX call 
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+    $("#js-gifDump").text(JSON.stringify(response));
+    gifDisplay();
+  });
+
+}
+
+// To display gifs
+function gifDisplay() {
+
+    $("#js-GifButtons").empty();
+
+    for (var i = 0; i < moods.length; i++) {
+    
+    var button = $("<button>");
+    button.addClass("newGif");
+    button.attr("gif-name", moods[i]);
+    button.text(moods[i]);
+    $("#js-GifButtons").append(button);
+            }
+          }
+
+
+ // This function handles events where one button is clicked
+ $("#add-mood").on("click", function(event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    let gifInput = $("#gif-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    moods.push(gifInput);
+    console.log(moods);
+
+    // Calling renderButtons which handles the processing of our movie array
+    gifDisplay();
+  });
+
+
+$(document).on("click", ".moodChoice", showTheGifs);
+    gifDisplay();
+
+
 // end of document ready 
 });
 
